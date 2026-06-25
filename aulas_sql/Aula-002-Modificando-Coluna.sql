db678813620b4336fc45cbb79ad783fbdca66207
ruans <ruansantossacramento32@gmail.com>
2026-06-25 17:15:55 -0300

    feat: adiciona aulas de order by, where e colunas calculadas

---
+++ aulas_sql/Aula-002-Modificando-Coluna.sql
@@ -0,0 +1,7 @@
+USE sakila;
+SELECT 
+    customer_id, 
+    amount, 
+    amount - (amount * 0.10) as '10% discount'
+from payment
+where customer_id = 1
--- aulas_sql/aula001/Aula-001-USE-SELECT-ORDERBY-WHERE.sql
+++
@@ -1,5 +0,0 @@
-
-select actor_id, first_name, last_name
-from actor
-where actor_id <= 9
--- order by last_namee;
